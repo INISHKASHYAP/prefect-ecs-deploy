296b34dc607bfcac3337bc3dace8175089724cee
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 13:05:39 +0530

    feat: Create public subnets in 3 AZs

--- main.tf
+++ main.tf
@@ -18,4 +18,14 @@ resource "aws_internet_gateway" "prefect_igw" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count = 3
+  vpc_id                  = aws_vpc.prefect_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.prefect_vpc.cidr_block, 8, count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "prefect-public-${count.index + 1}"
+  }
+}
 
