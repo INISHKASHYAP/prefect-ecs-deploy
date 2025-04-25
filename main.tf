9d2230784a2991d64f772824ba2aeac8efed3219
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 13:41:16 +0530

    Add IAM role for ECS Task Execution

--- main.tf
+++ main.tf
@@ -111,4 +111,23 @@ resource "aws_service_discovery_private_dns_namespace" "prefect_service_discover
   description = "Prefect service discovery namespace"
 }
 
+resource "aws_iam_role" "prefect_task_execution_role" {
+  name = "prefect-task-execution-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      Effect    = "Allow"
+      Sid       = ""
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.prefect_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECSTaskExecutionRolePolicy"
+}
 
