271676dbc95339856eeb796649547e01985d5ed8
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 13:37:18 +0530

    Add public and private route tables with associations

--- main.tf
+++ main.tf
@@ -63,3 +63,41 @@ resource "aws_nat_gateway" "prefect_nat_gateway" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.prefect_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.prefect_igw.id
+  }
+
+  tags = {
+    Name = "prefect-public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  count          = 3
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.prefect_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.prefect_nat_gateway.id
+  }
+
+  tags = {
+    Name = "prefect-private-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  count          = 3
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
