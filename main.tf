eed16f3ef60a90432e8aab12444cfb2685788d1a
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 13:09:04 +0530

    Create public subnets in 3 AZs

--- main.tf
+++ main.tf
@@ -29,3 +29,14 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_subnet" "private" {
+  count = 3
+  vpc_id                  = aws_vpc.prefect_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.prefect_vpc.cidr_block, 8, count.index + 3)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "prefect-private-${count.index + 1}"
+  }
+}
+
