9c8400d351dbfd18c1ed9579f864dfcc45a69080
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 14:28:14 +0530

    final tweaks in output file

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
+output "vpc_id" {
+  value = aws_vpc.prefect_vpc.id
+}
+
+output "public_subnets" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnets" {
+  value = aws_subnet.private[*].id
+}
 output "ecs_cluster_arn" {
   value = aws_ecs_cluster.prefect_cluster.arn
 }
