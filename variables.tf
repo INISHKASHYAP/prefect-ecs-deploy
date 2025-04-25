41de0aa6277e72dc6f907ae68eac6dd548977155
INISH KASHYAP <inishkash@gmail.com>
2025-04-25 14:27:52 +0530

    final tweaks in variable file

--- variables.tf
+++ variables.tf
@@ -1,5 +1,25 @@
-variable "prefect_api_key" {
-  description = "Prefect API key"
-  type        = string
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
 }
 
+variable "availability_zones" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "prefect_api_key" {
+  description = "Prefect Cloud API Key"
+  type        = string
+  sensitive   = true
+}
